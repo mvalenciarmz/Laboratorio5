1eed1e0a3f997724bb5e01a7346aa0115e938830
Marcos <marcos@Marcoss-Mac-mini.local>
2015-01-28 15:33:03 -0800

    Initial Commit

---
+++ Laboratorio5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Laboratorio5
+//
+//  Created by Marcos on 1/28/15.
+//  Copyright (c) 2015 Marcos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
